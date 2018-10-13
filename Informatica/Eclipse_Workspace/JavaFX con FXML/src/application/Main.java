6d9ffc99ce37d82b1e6d6221dfba13e4c92e0419
Davide Canal <canal.davide@gmail.com>
2018-10-13 22:42:19 +0200

    Caricamento progetto JavaFX con FXML e creazione del file .fxml

---
+++ Informatica/Eclipse_Workspace/JavaFX con FXML/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml")); //Carica il file .fxml 
+		Scene scene = new Scene(root, 400, 500);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("JavaFX con FXML");
+		primaryStage.show();
+	}
+}
