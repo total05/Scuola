71e62b8cbcbf171d8b462effaf85f76ab6c88321
Davide Canal <canaldavide@serverlab.local>
2018-11-16 13:03:48 +0100

    Tranforma in thread il socket udp

--- Informatica/Eclipse_Workspace/Socket_UDP/src/com/canalUdpClient/DatagramClient.java
+++ Informatica/Eclipse_Workspace/Socket_UDP/src/com/canalUdpClient/DatagramClient.java
@@ -4,19 +4,16 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class DatagramClient {
+public abstract class DatagramClient implements Runnable{
+	
+	DatagramSocket DS;
+	InetAddress IP;
+	String msg = "ciao";
+	DatagramPacket p = new DatagramPacket(msg.getBytes(), msg.getBytes().length, IP, 12345);
+		
 
 	public static void main(String[] args) throws Exception{
-		DatagramSocket DS = new DatagramSocket();
-		String msg = "ciao";
-		DatagramPacket p = new DatagramPacket(
-				msg.getBytes(),
-				msg.getBytes().length,
-				InetAddress.getByName("localhost"),
-				12345
-				);
-		DS.send(p);
-		DS.close();
+		
 	}
 
 }
